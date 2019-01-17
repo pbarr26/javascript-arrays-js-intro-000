17aff4a895e98b417ec648214c55dc9177dad2c5
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-17 19:36:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"] ;
 
 function addElementToBeginningOfArray(element, array) {
-  return [array, ...element]
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
